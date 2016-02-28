706dd5d647f47437ffd7d646c5e31af8a43cdbf2
Arjun Ahuja <Arjun@Apples-MacBook-Air-2.local>
2016-02-28 13:44:12 +0530

    adding view templates

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main.js')
-router.get('/',ctrlMain.index);
-
+var ctrlLocations = require('../controllers/locations.js')
+var ctrlOthers = require('../controllers/others.js')
+router.get('/',ctrlLocations.homelist);
+router.get('/locations',ctrlLocations.locationInfo);
+router.get('/locations/review/new',ctrlLocations.addReview);
+router.get('/about',ctrlOthers.about);
 module.exports = router;
